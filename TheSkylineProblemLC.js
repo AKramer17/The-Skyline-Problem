1bf96f19b367e939f4fa8c44c20f4446da4f47ce
Alex Kramer <kramer612@gmail.com>
2020-06-28 16:41:52 -0400

    Final

--- TheSkylineProblemLC.js
+++ TheSkylineProblemLC.js
@@ -11,14 +11,35 @@
  // DEF: Set ~= an array with no repeated elements
 var getSkyline = function(buildings) {
     if (buildings.length == 0) return [];
-};
 
-let lrHeightList = []; // Array for the left and right vertices of each building and the building height
 
-for (let building of buildings) {  // DEF: of 
+let lrhList = []; // Array for the left and right vertices of each building and the building height
+
+for (let building of buildings) {
     let [left, right, height] = building;  // 3 necessary values we need to know
-    lrHeightList.push([left, 0 - height]);
-    lrHeightList.push([right, height]);
+    lrhList.push([left, 0 - height]);
+    lrhList.push([right, height]);
 }
-// sort method sorts an array alphabetically
-lrHeightList.sort
+// DEF: sort == method that sorts an array alphabetically
+// If result is negative, a is sorted before b. If positive, b is sorted before a. If result is 0, no change to order.
+
+lrhList.sort((a, b) => a[0] === b[0] ? a[1] - b[1] : a[0] - b[0]);
+
+let result = [];
+let currentHeights = [0];
+let previousMaxHeight = 0;
+
+for (let i = 0; i < lrhList.length; i++) {
+    let [xpos, height] = lrhList[i];
+    if (height < 0) {
+        currentHeights.push(0 - height);
+    } else {
+        let removeIndex = currentHeights.indexOf(height);
+        currentHeights.splice(removeIndex, 1);
+    }
+    let currentMaxHeight = Math.max(...currentHeights);
+    if (currentMaxHeight != previousMaxHeight) result.push([xpos, currentMaxHeight])
+    previousMaxHeight = currentMaxHeight;
+}
+return result;
+};
