c28fbd74bede9507a49e1b9cd736c16406c8b40f
Alex Kramer <kramer612@gmail.com>
2020-06-28 15:32:51 -0400

    this is a hard question

--- TheSkylineProblemLC.js
+++ TheSkylineProblemLC.js
@@ -2,6 +2,10 @@
   @param {number[][]} buildings
   @return {number[][]}
  */
+
+ // You may assume all buildings are perfect rectangles grounded on an absolutely flat surface at height 0.
+
+ // DEF: Set ~= an array with no repeated elements
 var getSkyline = function(buildings) {
-    
+    if (buildings.length == 0) return [];
 };
