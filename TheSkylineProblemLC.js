8916fa6858cadca305c1a3ffc384113c5f800a3b
Alex Kramer <kramer612@gmail.com>
2020-06-28 16:04:44 -0400

    intro to sort

--- TheSkylineProblemLC.js
+++ TheSkylineProblemLC.js
@@ -5,13 +5,20 @@
 
  // You may assume all buildings are perfect rectangles grounded on an absolutely flat surface at height 0.
 
+
+
+
  // DEF: Set ~= an array with no repeated elements
 var getSkyline = function(buildings) {
     if (buildings.length == 0) return [];
 };
 
-let lrHeightList = []; // Array for the left and right vertices of each building
+let lrHeightList = []; // Array for the left and right vertices of each building and the building height
 
-for (let building of buildings) {
+for (let building of buildings) {  // DEF: of 
     let [left, right, height] = building;  // 3 necessary values we need to know
+    lrHeightList.push([left, 0 - height]);
+    lrHeightList.push([right, height]);
 }
+// sort method sorts an array alphabetically
+lrHeightList.sort
